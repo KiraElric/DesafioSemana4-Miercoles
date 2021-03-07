98524cb475e6a4897ee6b2f03f7ecbcd6e794833
KiraElric <lissette.urso@gmail.com>
2021-03-07 18:04:14 -0300

    Instrucciones en el archivo seed para poblar la base de datos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def dashboard
+    @projects = Project.all
   end
 
   def form
   end
 
   def create
+    @project = Project.create(name_project: params[:name_project], description: params[:description],
+                              start_on: params[:start_on], finish_on: params[:finish_on], state: params[:state])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Seeds ---> Forma de poblar la base de datos con registros "falsos", para tener información con la cual probar.
+
+30.times do |i|
+  @project = Project.create(name_project: "proyecto-#{i+1}", description: "Descripción del proyecto-#{i+1}",
+    start_on: Date.today - (i).days, finish_on: Date.today + (i).days , state: Project.states.keys.sample)
+end
+
+# Project.states.keys.sample --> Indicamos que tome al azar la llave del hash de todos los estados.
+#Se debe correr el comando rails db:seed para que tome las instrucciones de este archivo y poble la base de datos.
