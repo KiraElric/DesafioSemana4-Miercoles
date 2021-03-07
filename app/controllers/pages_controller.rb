ff52d4b5548955588eabac257388a68053016bea
KiraElric <lissette.urso@gmail.com>
2021-03-07 20:13:33 -0300

    Adicion del filtro, el select del formulario y arreglo de bugs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
 class PagesController < ApplicationController
+  http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :dashboard
   def dashboard
-    @projects = Project.all
+    # Forma-1 de buscar a partir del parametro que entrega el link del dashboard.
+    #if params[:s] == ""
+    #  @projects = Project.all
+    #elsif params[:s] == "proposal"
+    #  @projects = Project.search_proposal
+    #elsif params[:s] == "in_progress"
+    #  @projects = Project.search_in_progress
+    #elsif params[:s] == "finished"
+    #  @projects = Project.search_finished
+    #end
+
+    #Forma-2 de buscar a partir del parametro que entrega el link del dashboard
+    #Se establece que retorne todos los registros a menos que el parametro s tenga una llave y este presente
+    return @projects = Project.all unless params.has_key?(:s) && params[:s].present?
+    #Si la condicion anterior no se cumple, avanzara a esta donde se buscara segun el parametro s recibido (scope)
+    @projects = Project.search(params[:s])
   end
 
   def form
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,8 @@
 class Project < ApplicationRecord
+  validates :name_project, :description, :state, presence: true
   enum state: [:proposal, :in_progress, :finished] #Arreglo de simbolos con los estados del proyecto.
+  # scope :search_proposal, ->{where(state: 'proposal')} --> Scope de busqueda para el estado de propuesta
+  # scope :search_in_progress, ->{where(state: 'in_progress')} --> Scope de busqueda para el estado de en progreso
+  # scope :search_finished, ->{where(state: 'finished')} --> Scope de busqueda para el estado de terminado
+  scope :search, ->(param) {where(state: param )} #Scope de busqueda según un parametro que recibira desde una vista (forma mas génerica de busqueda que las definidas arriba)
 end
